92af44fefefb46220f816cc0ecb8256eb8e625b0
Lasha Skhirtladze <altrix5@yahoo.com>
2022-12-29 19:08:00 +0400

    --

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const httpProxy = require('http-proxy');
 
 // Create a proxy and listen on port 3000
-const proxy = httpProxy.createProxyServer({});
+// const proxy = httpProxy.createProxyServer({});
 const app = express();
-app.get('*', function(req, res) {
-  // Prints "Request GET https://httpbin.org/get?answer=42"
-  console.log('Request', req.method, req.url);
-  proxy.web(req, res, { target: `${req.protocol}://${req.hostname}` });
-});
+// app.get('*', function(req, res) {
+//   // Prints "Request GET https://httpbin.org/get?answer=42"
+//   console.log('Request', req.method, req.url);
+//   proxy.web(req, res, { target: `${req.protocol}://${req.hostname}` });
+// });
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
