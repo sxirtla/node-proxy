10a97a3392796ebf99b060fa7d92a8716f142d4f
Lasha Skhirtladze <altrix5@yahoo.com>
2022-12-29 18:47:35 +0400

    .

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.get('*', function(req, res) {
   console.log('Request', req.method, req.url);
   proxy.web(req, res, { target: `${req.protocol}://${req.hostname}` });
 });
-const server = app.listen(3000);
+app.listen(process.env.PORT || 3000)
